7b06906f77ac6fe6e04c4ce1e6413325c25aaff5
Jonas <airfryerkitten@gmail.com>
2026-02-24 10:26:15 -0500

    Adding rough keys

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function genKeyboard(){
     const letters = "abcdefghijklmnopqrstuvwxyz"
-    
+    const keyboard = document.getElementById("keyboard")
 
     for (let i = 0; i < 25; i++){
-        
+        const div = document.createElement("div")
+        div.textContent = letters[i]
+        keyboard.appendChild(div)
     }
 }
