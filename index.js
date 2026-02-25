04771e758217bc0e326bcd36e4aab0443b09f844
Jonas <airfryerkitten@gmail.com>
2026-02-25 10:20:06 -0500

    Added phrases

--- index.js
+++ index.js
@@ -18,3 +18,27 @@ function genKeyboard(){
         pointer++
     }
 }
+
+function genPhrase(){
+    const phrases = [`I'm afraid you're ratted your last tatouille`, 
+        "my apolocheese", 
+        "Sniff sniff sniff hello sniff sniff hi hi sniff",
+        "Crazy? I was crazy once",
+        "Excuse me, Do you have time to talk about our lord and saviour cheesus", 
+        "Rat clenches Fist of Rage", 
+        "SNFNFSN SNNNFFFFF SNIFF SNIFF SNIFF SNFNFNFNF",
+        `We're not on squeaking terms rn`,
+        "Rats are not a mice thing to have in your house.",
+        "Live, Laugh, cheese"]
+
+        const n = Math.floor(Math.random() * phrases.length)
+
+    const random_phrase = phrases[n];
+    const phrasetext = document.getElementById("phraseText");
+    console.log(random_phrase)
+    phrasetext.textContent = random_phrase;
+
+
+
+
+}
