8bf8b7dd17e6089e0908b2366fe70a416cf6a7c1
Jonas <airfryerkitten@gmail.com>
2026-02-25 09:25:56 -0500

    Adding Keyboard Keys and Rows

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 function genKeyboard(){
-    const letters = "abcdefghijklmnopqrstuvwxyz"
+    const letters = "abcdef*ghijkl*mnopqr*stuvwx*yz"
     const keyboard = document.getElementById("keyboard")
+    let pointer = 0;
 
-    for (let i = 0; i < 25; i++){
-        const div = document.createElement("div")
-        div.textContent = letters[i]
-        keyboard.appendChild(div)
+    for (let i = 0; i < 6; i++){
+        const row = document.createElement("div");
+        row.className = "keyboardRow";
+        while(letters[pointer] != "*" && pointer < 30){
+            const key = document.createElement("div");
+            key.className = "keyboardKey";
+            key.id = letters[pointer];
+            key.textContent = letters[pointer].toUpperCase();
+            row.appendChild(key);
+            pointer++;
+        }
+        keyboard.append(row);
+        pointer++
     }
 }
